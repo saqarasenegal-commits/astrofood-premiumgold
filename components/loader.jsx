9a57097b635cd27846fd219fdfbe9607c4ec5c7d
saqarasenegal-commits <saqarasenegal@gmail.com>
2025-11-11 20:13:42 +0300

    Create loader.jsx

---
+++ components/loader.jsx
@@ -0,0 +1,8 @@
+export default function Loader() {
+  return (
+    <div className="flex flex-col items-center justify-center text-yellow-300 animate-pulse py-10">
+      <div className="text-4xl">🔮</div>
+      <p className="mt-2 text-lg font-semibold">Connexion aux étoiles...</p>
+    </div>
+  );
+}
